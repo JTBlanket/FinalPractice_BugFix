55ad4b9be2f9085f78c49bacbba7573f17a1e4be
JTBlanket <j.blankenship1717@gmail.com>
2017-05-17 12:13:35 -0600

    Added JOP welcome message

--- src/runner.java
+++ src/runner.java
@@ -1,8 +1,12 @@
+import javax.swing.JOptionPane;
 
 public class runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		runner myRun = new runner();
+		Converter myConverter = new Converter();
+		JOptionPane.showMessageDialog(null,"Welcome, this program converts x to y");
+		
 
 	}
 
